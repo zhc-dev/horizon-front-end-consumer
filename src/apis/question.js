2244535f9bf08e53954d5a49229a132c376196f9
zhc-dev <zhc.1118.dev@gmail.com>
2025-04-15 21:46:24 +0800

    feat:题目列表

---
+++ src/apis/question.js
@@ -0,0 +1,48 @@
+import service from "@/utils/request";
+
+export function getQuestionListService(params) {
+  return service({
+    url: "/question/semi/login/list",
+    method: "get",
+    params,
+  });
+}
+
+export function getHotQuestionListService() {
+  return service({
+    url: "/question/semiLogin/hotList",
+    method: "get"
+  });
+}
+
+export function getQuestionDetailService(questionId) {
+  return service({
+    url: "/question/detail",
+    method: "get",
+    params: { questionId },
+  });
+}
+
+export function preQuestionService(questionId) {
+  return service({
+    url: "/question/preQuestion",
+    method: "get",
+    params: { questionId },
+  });
+}
+
+export function nextQuestionService(questionId) {
+  return service({
+    url: "/question/nextQuestion",
+    method: "get",
+    params: { questionId },
+  });
+}
+
+export function getQuestionResultService(examId, questionId, currentTime) {
+  return service({
+    url: "/user/question/exe/result",
+    method: "get",
+    params: { examId, questionId, currentTime }
+  });
+}
